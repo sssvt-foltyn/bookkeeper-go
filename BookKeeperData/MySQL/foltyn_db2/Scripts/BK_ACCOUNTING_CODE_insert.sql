cce8e76cb907aadd36216cdd879e7adf350f6d6e
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-05-13 12:30:33 +0200

    CHANGE Add data for BK_ACCOUNTING_CODE and BK_CONTACT.

--- BookKeeperData/MySQL/foltyn_db2/Scripts/BK_ACCOUNTING_CODE_insert.sql
+++ BookKeeperData/MySQL/foltyn_db2/Scripts/BK_ACCOUNTING_CODE_insert.sql
@@ -18,3 +18,219 @@ INSERT INTO
     TRUE                                                 AS IS_TAXABLE
 ;
 
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 2              AS ID,
+    'PS'                                                 AS CODE,
+    'příjmy - služby'                                    AS NAME,
+    'Příjmy zahrnované do základu daně - prodej výrobků a služeb'   AS NOTE,
+    TRUE                                                 AS IS_TAXABLE
+;
+
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 3              AS ID,
+    'PO'                                                 AS CODE,
+    'příjmy - ostatní'                                   AS NAME,
+    'Příjmy zahrnované do základu daně - ostatní'        AS NOTE,
+    TRUE                                                 AS IS_TAXABLE
+;
+
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 4              AS ID,
+    'VM'                                                 AS CODE,
+    'výdaje - materiál'                                  AS NAME,
+    'Výdaje zahrnované do základu daně - nákup materiálu'   AS NOTE,
+    TRUE                                                 AS IS_TAXABLE
+;
+
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 5              AS ID,
+    'VZ'                                                 AS CODE,
+    'výdaje - zboží'                                     AS NAME,
+    'Výdaje zahrnované do základu daně - nákup zboží'    AS NOTE,
+    TRUE                                                 AS IS_TAXABLE
+;
+
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 6              AS ID,
+    'VD'                                                 AS CODE,
+    'výdaje - mzdy'                                      AS NAME,
+    'Výdaje zahrnované do základu daně - mzdy'           AS NOTE,
+    TRUE                                                 AS IS_TAXABLE
+;
+
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 7              AS ID,
+    'VP'                                                 AS CODE,
+    'výdaje - pojištění'                                 AS NAME,
+    'Výdaje zahrnované do základu daně - pojištění, sociální a zdravotní'   AS NOTE,
+    TRUE                                                 AS IS_TAXABLE
+;
+
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 8              AS ID,
+    'VV'                                                 AS CODE,
+    'výdaje - vybavení'                                  AS NAME,
+    'Výdaje zahrnované do základu daně - nákup vybavení a kancelářských potřeb'   AS NOTE,
+    TRUE                                                 AS IS_TAXABLE
+;
+
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 9              AS ID,
+    'VR'                                                 AS CODE,
+    'výdaje - režie'                                     AS NAME,
+    'Výdaje zahrnované do základu daně - provozní režie' AS NOTE,
+    TRUE                                                 AS IS_TAXABLE
+;
+
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 10              AS ID,
+    'NVV'                                                 AS CODE,
+    'nezapočítávat - vklady a výběry'                     AS NAME,
+    'Příjmy a výdaje nezahrnované do základu daně - osobní vklady a výběry (+/-)'   AS NOTE,
+    FALSE                                                 AS IS_TAXABLE
+;
+
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 11              AS ID,
+    'NUS'                                                 AS CODE,
+    'nezapočítávat - úvěr a splátky'                      AS NAME,
+    'Příjmy a výdaje nezahrnované do základu daně - úvěr a splátky (+/-)'   AS NOTE,
+    FALSE                                                 AS IS_TAXABLE
+;
+
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 14              AS ID,
+    'NJP'                                                 AS CODE,
+    'nezapočítávat - jiné příjmy'                         AS NAME,
+    'Příjmy a výdaje nezahrnované do základu daně - ostatní příjmy'   AS NOTE,
+    FALSE                                                 AS IS_TAXABLE
+;
+
+
+INSERT INTO
+  BK_ACCOUNTING_CODE
+  (
+    -- ID,
+    CODE,
+    NAME,
+    NOTE,
+    IS_TAXABLE
+  )
+  SELECT
+    -- 15              AS ID,
+    'NJV'                                                 AS CODE,
+    'nezapočítávat - jiné výdaje'                         AS NAME,
+    'Příjmy a výdaje nezahrnované do základu daně - ostatní výdaje'   AS NOTE,
+    FALSE                                                 AS IS_TAXABLE
+;
+
---
+++ BookKeeperData/MySQL/foltyn_db2/Scripts/BK_CONTACT_insert.sql
@@ -0,0 +1,257 @@
+USE foltyn_db2;
+
+
+INSERT INTO
+  BK_CONTACT
+  (
+    -- ID,
+    NAME,
+    ICO,
+    DIC,
+    BANK_ACCOUNT,
+    MOBILE,
+    EMAIL,
+    WWW,
+    ADDRESS_STREET,
+    ADDRESS_CITY,
+    ADDRESS_ZIP,
+    ADDRESS_COUNTRY
+  )
+  SELECT
+    -- 1                                AS ID,
+    'Alza.cz a.s.'                   AS NAME,
+    '27082440'                       AS ICO,
+    'CZ27082440'                     AS DIC,
+    '1017560164/6100'                AS BANK_ACCOUNT,
+    NULL                             AS MOBILE,
+    'info@alza.cz'                   AS EMAIL,
+    'www.alza.cz'                    AS WWW,
+    'Jankovcova 1522/53'             AS ADDRESS_STREET,
+    'Praha'                          AS ADDRESS_CITY,
+    '17000'                          AS ADDRESS_ZIP,
+    'CZ'                             AS ADDRESS_COUNTRY
+;
+
+
+INSERT INTO
+  BK_CONTACT
+  (
+    -- ID,
+    NAME,
+    ICO,
+    DIC,
+    BANK_ACCOUNT,
+    MOBILE,
+    EMAIL,
+    WWW,
+    ADDRESS_STREET,
+    ADDRESS_CITY,
+    ADDRESS_ZIP,
+    ADDRESS_COUNTRY
+  )
+  SELECT
+    -- 2                                AS ID,
+    'Plzeňský Prazdroj, a. s.'       AS NAME,
+    '45357366'                       AS ICO,
+    'CZ45357366'                     AS DIC,
+    '2029990203/2600'                AS BANK_ACCOUNT,
+    NULL                             AS MOBILE,
+    'info@prazdroj.cz'               AS EMAIL,
+    'www.prazdroj.cz'                AS WWW,
+    'U Prazdroje 64/7'               AS ADDRESS_STREET,
+    'Plzeň'                          AS ADDRESS_CITY,
+    '30100'                          AS ADDRESS_ZIP,
+    'CZ'                             AS ADDRESS_COUNTRY
+;
+
+
+INSERT INTO
+  BK_CONTACT
+  (
+    -- ID,
+    NAME,
+    ICO,
+    DIC,
+    BANK_ACCOUNT,
+    MOBILE,
+    EMAIL,
+    WWW,
+    ADDRESS_STREET,
+    ADDRESS_CITY,
+    ADDRESS_ZIP,
+    ADDRESS_COUNTRY
+  )
+  SELECT
+    -- 3                                AS ID,
+    'Tesco Franchise Stores ČR s.r.o.'    AS NAME,
+    '04621611'                       AS ICO,
+    'CZ04621611'                     AS DIC,
+    '2058360104/2600'                AS BANK_ACCOUNT,
+    '+420 731 672 019'               AS MOBILE,
+    'info@izabka.cz'                 AS EMAIL,
+    'www.izabka.cz'                  AS WWW,
+    'Vršovická 1527/68b'             AS ADDRESS_STREET,
+    'Praha'                          AS ADDRESS_CITY,
+    '10000'                          AS ADDRESS_ZIP,
+    'CZ'                             AS ADDRESS_COUNTRY
+;
+
+
+INSERT INTO
+  BK_CONTACT
+  (
+    -- ID,
+    NAME,
+    ICO,
+    DIC,
+    BANK_ACCOUNT,
+    MOBILE,
+    EMAIL,
+    WWW,
+    ADDRESS_STREET,
+    ADDRESS_CITY,
+    ADDRESS_ZIP,
+    ADDRESS_COUNTRY
+  )
+  SELECT
+    -- 4                                AS ID,
+    'ORLEN Unipetrol RPA s.r.o.'     AS NAME,
+    '27597075'                       AS ICO,
+    'CZ27597075'                     AS DIC,
+    NULL                             AS BANK_ACCOUNT,
+    '+420 225 001 444'               AS MOBILE,
+    'benzina@benzina.cz'             AS EMAIL,
+    'www.benzina.cz'                 AS WWW,
+    'Litvínov 1'                     AS ADDRESS_STREET,
+    'Litvínov'                       AS ADDRESS_CITY,
+    '43601'                          AS ADDRESS_ZIP,
+    'CZ'                             AS ADDRESS_COUNTRY
+;
+
+
+INSERT INTO
+  BK_CONTACT
+  (
+    -- ID,
+    NAME,
+    ICO,
+    DIC,
+    BANK_ACCOUNT,
+    MOBILE,
+    EMAIL,
+    WWW,
+    ADDRESS_STREET,
+    ADDRESS_CITY,
+    ADDRESS_ZIP,
+    ADDRESS_COUNTRY
+  )
+  SELECT
+    -- 5                                AS ID,
+    'NEW IMAGE PRODUCTION s.r.o.'    AS NAME,
+    '27916880'                       AS ICO,
+    'CZ27916880'                     AS DIC,
+    '35-9307590237/0100'             AS BANK_ACCOUNT,
+    NULL                             AS MOBILE,
+    'nip@imagein.cz'                 AS EMAIL,
+    'www.helenabedrnova.cz'          AS WWW,
+    '28. pluku 524/25'               AS ADDRESS_STREET,
+    'Praha'                          AS ADDRESS_CITY,
+    '10100'                          AS ADDRESS_ZIP,
+    'CZ'                             AS ADDRESS_COUNTRY
+;
+
+
+INSERT INTO
+  BK_CONTACT
+  (
+    -- ID,
+    NAME,
+    ICO,
+    DIC,
+    BANK_ACCOUNT,
+    MOBILE,
+    EMAIL,
+    WWW,
+    ADDRESS_STREET,
+    ADDRESS_CITY,
+    ADDRESS_ZIP,
+    ADDRESS_COUNTRY
+  )
+  SELECT
+    -- 6                                AS ID,
+    'ŠKODA AUTO a.s.'                AS NAME,
+    '00177041'                       AS ICO,
+    'CZ00177041'                     AS DIC,
+    '1387698195/2700'                AS BANK_ACCOUNT,
+    '+420 326 811 111'               AS MOBILE,
+    'infoline@skoda-auto.cz'         AS EMAIL,
+    'www.skoda-auto.cz'              AS WWW,
+    'tř. Václava Klementa 869'       AS ADDRESS_STREET,
+    'Mladá Boleslav'                 AS ADDRESS_CITY,
+    '29301'                          AS ADDRESS_ZIP,
+    'CZ'                             AS ADDRESS_COUNTRY
+;
+
+
+INSERT INTO
+  BK_CONTACT
+  (
+    -- ID,
+    NAME,
+    ICO,
+    DIC,
+    BANK_ACCOUNT,
+    MOBILE,
+    EMAIL,
+    WWW,
+    ADDRESS_STREET,
+    ADDRESS_CITY,
+    ADDRESS_ZIP,
+    ADDRESS_COUNTRY
+  )
+  SELECT
+    -- 7                                AS ID,
+    'Halla, a.s.'                    AS NAME,
+    '60201916'                       AS ICO,
+    'CZ60201916'                     AS DIC,
+    '553219002/5500'                 AS BANK_ACCOUNT,
+    '+420 286 880 161'               AS MOBILE,
+    'prodej@halla.cz'                AS EMAIL,
+    'www.halla.cz'                   AS WWW,
+    'Litvínovská 288/11'             AS ADDRESS_STREET,
+    'Praha'                          AS ADDRESS_CITY,
+    '19000'                          AS ADDRESS_ZIP,
+    'CZ'                             AS ADDRESS_COUNTRY
+;
+
+
+INSERT INTO
+  BK_CONTACT
+  (
+    -- ID,
+    NAME,
+    ICO,
+    DIC,
+    BANK_ACCOUNT,
+    MOBILE,
+    EMAIL,
+    WWW,
+    ADDRESS_STREET,
+    ADDRESS_CITY,
+    ADDRESS_ZIP,
+    ADDRESS_COUNTRY
+  )
+  SELECT
+    -- 8                                AS ID,
+    'JMC Machinery Sales, Inc.'      AS NAME,
+    NULL                             AS ICO,
+    NULL                             AS DIC,
+    NULL                             AS BANK_ACCOUNT,
+    '+1 586-756-8855'                AS MOBILE,
+    'jack@jmcmachinerysales.com'     AS EMAIL,
+    'www.jmcmachinerysales.com'      AS WWW,
+    '11430 Kaltz Avenue'             AS ADDRESS_STREET,
+    'Warren, Michigan'               AS ADDRESS_CITY,
+    '48089'                          AS ADDRESS_ZIP,
+    'US'                             AS ADDRESS_COUNTRY
+;
