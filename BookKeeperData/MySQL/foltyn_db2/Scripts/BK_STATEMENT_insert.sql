d883173d7bc891da07c86f797569c614ecd0b150
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-05-13 14:57:57 +0200

    CHANGE Add data for BK_STATEMENT.

---
+++ BookKeeperData/MySQL/foltyn_db2/Scripts/BK_STATEMENT_insert.sql
@@ -0,0 +1,217 @@
+USE foltyn_db2;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 1                       AS ID,
+    2020                    AS YEAR,
+    1                       AS STATEMENT_NUMBER,
+    '2020-01-31'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 2                       AS ID,
+    2020                    AS YEAR,
+    2                       AS STATEMENT_NUMBER,
+    '2020-02-29'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 3                       AS ID,
+    2020                    AS YEAR,
+    3                       AS STATEMENT_NUMBER,
+    '2020-03-31'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 4                       AS ID,
+    2020                    AS YEAR,
+    4                       AS STATEMENT_NUMBER,
+    '2020-04-30'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 5                       AS ID,
+    2020                    AS YEAR,
+    5                       AS STATEMENT_NUMBER,
+    '2020-05-31'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 6                       AS ID,
+    2020                    AS YEAR,
+    6                       AS STATEMENT_NUMBER,
+    '2020-06-30'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 7                       AS ID,
+    2020                    AS YEAR,
+    7                       AS STATEMENT_NUMBER,
+    '2020-07-31'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 8                       AS ID,
+    2020                    AS YEAR,
+    8                       AS STATEMENT_NUMBER,
+    '2020-08-31'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 9                       AS ID,
+    2020                    AS YEAR,
+    9                       AS STATEMENT_NUMBER,
+    '2020-09-30'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 10                      AS ID,
+    2020                    AS YEAR,
+    10                      AS STATEMENT_NUMBER,
+    '2020-10-31'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 11                      AS ID,
+    2020                    AS YEAR,
+    11                      AS STATEMENT_NUMBER,
+    '2020-11-30'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
+
+
+INSERT INTO
+  BK_STATEMENT
+  (
+    -- ID,
+    YEAR,
+    STATEMENT_NUMBER,
+    DATE_ISSUED,
+    IS_BOOKED
+  )
+  SELECT
+    -- 12                      AS ID,
+    2020                    AS YEAR,
+    12                      AS STATEMENT_NUMBER,
+    '2020-12-31'            AS DATE_ISSUED,
+    TRUE                    AS IS_BOOKED
+;
