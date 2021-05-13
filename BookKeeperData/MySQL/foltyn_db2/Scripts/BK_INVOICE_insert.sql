45c01c51f17417048ed07dcd489692b8883578d2
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-05-13 12:54:59 +0200

    CHANGE Add data for BK_INVOICE.

--- BookKeeperData/MySQL/foltyn_db2/Scripts/BK_INVOICE_insert.sql
+++ BookKeeperData/MySQL/foltyn_db2/Scripts/BK_INVOICE_insert.sql
@@ -5,18 +5,20 @@ INSERT INTO
   BK_INVOICE
   (
     -- ID,
-    INVOICE_NUMBER,
     YEAR,
-    COMPANY_NAME,
+    DIRECTION,
+    INVOICE_NUMBER,
+    CONTACT_ID,
     TOTAL_AMOUNT,
     DATE_ISSUED,
     DATE_DUE
   )
   SELECT
     -- 1               AS ID,
-    1                             AS INVOICE_NUMBER,
     2021                          AS YEAR,
-    'IBM'                         AS COMPANY_NAME,
+    'Outgoing'                    AS DIRECTION,
+    '001-2021'                    AS INVOICE_NUMBER,
+    7                             AS CONTACT_ID,
     520.40                        AS TOTAL_AMOUNT,
     '2021-01-03'                  AS DATE_ISSUED,
     '2021-01-24'                  AS DATE_DUE
@@ -27,18 +29,20 @@ INSERT INTO
   BK_INVOICE
   (
     -- ID,
-    INVOICE_NUMBER,
     YEAR,
-    COMPANY_NAME,
+    DIRECTION,
+    INVOICE_NUMBER,
+    CONTACT_ID,
     TOTAL_AMOUNT,
     DATE_ISSUED,
     DATE_DUE
   )
   SELECT
     -- 2               AS ID,
-    2                             AS INVOICE_NUMBER,
     2021                          AS YEAR,
-    'Microsoft'                   AS COMPANY_NAME,
+    'Outgoing'                    AS DIRECTION,
+    '002-2021'                    AS INVOICE_NUMBER,
+    6                             AS CONTACT_ID,
     1011.20                       AS TOTAL_AMOUNT,
     '2021-01-17'                  AS DATE_ISSUED,
     '2021-02-07'                  AS DATE_DUE
@@ -49,18 +53,20 @@ INSERT INTO
   BK_INVOICE
   (
     -- ID,
-    INVOICE_NUMBER,
     YEAR,
-    COMPANY_NAME,
+    DIRECTION,
+    INVOICE_NUMBER,
+    CONTACT_ID,
     TOTAL_AMOUNT,
     DATE_ISSUED,
     DATE_DUE
   )
   SELECT
     -- 3               AS ID,
-    3                             AS INVOICE_NUMBER,
     2021                          AS YEAR,
-    'Alza'                         AS COMPANY_NAME,
+    'Incoming'                    AS DIRECTION,
+    '8473747'                     AS INVOICE_NUMBER,
+    1                             AS CONTACT_ID,
     490.00                        AS TOTAL_AMOUNT,
     '2021-01-29'                  AS DATE_ISSUED,
     '2021-02-19'                  AS DATE_DUE
@@ -71,18 +77,20 @@ INSERT INTO
   BK_INVOICE
   (
     -- ID,
-    INVOICE_NUMBER,
     YEAR,
-    COMPANY_NAME,
+    DIRECTION,
+    INVOICE_NUMBER,
+    CONTACT_ID,
     TOTAL_AMOUNT,
     DATE_ISSUED,
     DATE_DUE
   )
   SELECT
     -- 4               AS ID,
-    4                             AS INVOICE_NUMBER,
     2021                          AS YEAR,
-    'IBM'                         AS COMPANY_NAME,
+    'Outgoing'                    AS DIRECTION,
+    '003-2021'                    AS INVOICE_NUMBER,
+    7                             AS COMPANY_NAME,
     110.30                        AS TOTAL_AMOUNT,
     '2021-02-16'                  AS DATE_ISSUED,
     '2021-03-09'                  AS DATE_DUE
@@ -93,18 +101,20 @@ INSERT INTO
   BK_INVOICE
   (
     -- ID,
-    INVOICE_NUMBER,
     YEAR,
-    COMPANY_NAME,
+    DIRECTION,
+    INVOICE_NUMBER,
+    CONTACT_ID,
     TOTAL_AMOUNT,
     DATE_ISSUED,
     DATE_DUE
   )
   SELECT
     -- 5               AS ID,
-    5                             AS INVOICE_NUMBER,
     2021                          AS YEAR,
-    'Albert'                         AS COMPANY_NAME,
+    'Incoming'                    AS DIRECTION,
+    '2021079'                     AS INVOICE_NUMBER,
+    3                             AS CONTACT_ID,
     343.20                        AS TOTAL_AMOUNT,
     '2021-02-21'                  AS DATE_ISSUED,
     '2021-03-21'                  AS DATE_DUE
@@ -115,19 +125,21 @@ INSERT INTO
   BK_INVOICE
   (
     -- ID,
-    INVOICE_NUMBER,
     YEAR,
-    COMPANY_NAME,
+    DIRECTION,
+    INVOICE_NUMBER,
+    CONTACT_ID,
     TOTAL_AMOUNT,
     DATE_ISSUED,
     DATE_DUE
   )
   SELECT
     -- 6               AS ID,
-    6                             AS INVOICE_NUMBER,
     2021                          AS YEAR,
-    'Rohlík.cz'                         AS COMPANY_NAME,
-    1410.65                        AS TOTAL_AMOUNT,
+    'Incoming'                    AS DIRECTION,
+    '2021085'                     AS INVOICE_NUMBER,
+    3                             AS COMPANY_NAME,
+    1410.65                       AS TOTAL_AMOUNT,
     '2021-03-15'                  AS DATE_ISSUED,
     '2021-04-05'                  AS DATE_DUE
 ;
@@ -137,19 +149,21 @@ INSERT INTO
   BK_INVOICE
   (
     -- ID,
-    INVOICE_NUMBER,
     YEAR,
-    COMPANY_NAME,
+    DIRECTION,
+    INVOICE_NUMBER,
+    CONTACT_ID,
     TOTAL_AMOUNT,
     DATE_ISSUED,
     DATE_DUE
   )
   SELECT
     -- 7               AS ID,
-    7                             AS INVOICE_NUMBER,
     2021                          AS YEAR,
-    'Rohlík.cz'                         AS COMPANY_NAME,
-    2900.48                        AS TOTAL_AMOUNT,
+    'Incoming'                    AS DIRECTION,
+    '2021103'                     AS INVOICE_NUMBER,
+    3                             AS COMPANY_NAME,
+    2900.48                       AS TOTAL_AMOUNT,
     '2021-03-19'                  AS DATE_ISSUED,
     '2021-04-19'                  AS DATE_DUE
 ;
