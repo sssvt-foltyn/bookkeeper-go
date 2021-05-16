eefc709abc646bcc4f416ef57cb391689db8b7cf
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-05-16 16:33:02 +0200

    TECH Change configuration (disable SSL). Fix future CORS issues in Startup.

--- BookKeeperBE/BookKeeperBESol/BookKeeperBERest/Startup.cs
+++ BookKeeperBE/BookKeeperBESol/BookKeeperBERest/Startup.cs
@@ -6,26 +6,63 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+
 namespace BookKeeperBERest
 {
+
+
+
     public class Startup
     {
+
+
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+
+
         public IConfiguration Configuration { get; }
+
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            // Fix the CORS issue.
+            // Chrome:
+            // Access to XMLHttpRequest at 'http://localhost:10789/api/heroes' from origin 'http://localhost:4200' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+            // Firefox:
+            // Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at http://localhost:10789/api/heroes. (Reason: CORS header ‘Access-Control-Allow-Origin’ missing)
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        // The URL of http://localhost:4200 is the Angular app (FE).
+                        builder.WithOrigins("http://localhost:4200")
+                            // Another CORS issue.
+                            // Firefox:
+                            // Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at http://localhost:10789/api/heroes. (Reason: Did not find method in CORS header ‘Access-Control-Allow-Methods’).
+                            // Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at http://localhost:10789/api/heroes. (Reason: CORS request did not succeed).
+                            // Object { headers: {…}, status: 0, statusText: "Unknown Error", url: "http://localhost:10789/api/heroes", ok: false, name: "HttpErrorResponse", message: "Http failure response for http://localhost:10789/api/heroes: 0 Unknown Error", error: error }
+                            .WithMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+                            .WithHeaders(HeaderNames.ContentType)
+                            ;
+                    });
+            }
+            );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -33,6 +70,8 @@ namespace BookKeeperBERest
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookKeeperBERest", Version = "v1" });
             });
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -48,6 +87,16 @@ namespace BookKeeperBERest
 
             app.UseRouting();
 
+            // BE: http://localhost:10789/api/heroes (this app)
+            // FE: http://localhost:4200 (Angular)
+            // ***
+            // Fix the CORS issue.
+            // Chrome:
+            // Access to XMLHttpRequest at 'http://localhost:10789/api/heroes' from origin 'http://localhost:4200' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+            // Firefox:
+            // Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at http://localhost:10789/api/heroes. (Reason: CORS header ‘Access-Control-Allow-Origin’ missing)
+            app.UseCors();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -55,5 +104,11 @@ namespace BookKeeperBERest
                 endpoints.MapControllers();
             });
         }
+
+
+
     }
+
+
+
 }
