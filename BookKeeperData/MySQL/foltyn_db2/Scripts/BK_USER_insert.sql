8f1b1a4dadd06b3d93b5bf8f09994469f8fc7ed7
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-05-17 04:36:43 +0200

    CHANGE Add more users to BK_USER.

--- BookKeeperData/MySQL/foltyn_db2/Scripts/BK_USER_insert.sql
+++ BookKeeperData/MySQL/foltyn_db2/Scripts/BK_USER_insert.sql
@@ -27,3 +27,73 @@ INSERT INTO
     'nasta'        AS USERNAME,
     'abc'          AS PASSWORD
 ;
+
+
+INSERT INTO
+  BK_USER
+  (
+    -- ID,
+    USERNAME,
+    PASSWORD
+  )
+  SELECT
+    -- 3              AS ID,
+    'baba'         AS USERNAME,
+    'yaga'         AS PASSWORD
+;
+
+
+INSERT INTO
+  BK_USER
+  (
+    -- ID,
+    USERNAME,
+    PASSWORD
+  )
+  SELECT
+    -- 4              AS ID,
+    'mara'        AS USERNAME,
+    'ar'          AS PASSWORD
+;
+
+
+INSERT INTO
+  BK_USER
+  (
+    -- ID,
+    USERNAME,
+    PASSWORD
+  )
+  SELECT
+    -- 5              AS ID,
+    'sempa'        AS USERNAME,
+    'aaa'          AS PASSWORD
+;
+
+
+INSERT INTO
+  BK_USER
+  (
+    -- ID,
+    USERNAME,
+    PASSWORD
+  )
+  SELECT
+    -- 6              AS ID,
+    'kulak'        AS USERNAME,
+    'bbb'          AS PASSWORD
+;
+
+
+INSERT INTO
+  BK_USER
+  (
+    -- ID,
+    USERNAME,
+    PASSWORD
+  )
+  SELECT
+    -- 7              AS ID,
+    'kuki'         AS USERNAME,
+    'ccc'          AS PASSWORD
+;
